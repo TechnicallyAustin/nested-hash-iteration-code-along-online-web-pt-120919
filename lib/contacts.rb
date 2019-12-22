3a3e8e7b80c95801f18b7551dc8d05e92900ffd2
Austin Johnson <technicallyaustin@gmail.com>
2019-12-22 23:51:41 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,9 +16,9 @@ require 'pry'
   
 def remove_strawberry(contacts)
 contacts.each do |name, data|
-  binding.pry
   "#{name}: #{data}"
   contacts.each do |attribute, value|
+    binding.pry
     "#{attribute}: #{value}"
     if attribute == favorite_ice_cream_flavors
       
